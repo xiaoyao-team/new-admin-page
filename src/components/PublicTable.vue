f90b9db3e7f96d4545d9f814d5a2dbb21edb80bd
tao.wang <tao.wang@xyroyale.com>
2021-03-18 18:10:27 +0800

    ~~

---
+++ src/components/PublicTable.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <el-table
+      :data="tableData"
+      style="width: 100%"
+      border
+      :default-sort="defaultSort"
+      max-height="550">
+      <el-table-column
+        type="index"
+        width="50">
+      </el-table-column>
+      <el-table-column
+        v-for="(item,index) in tableHeader"
+        :key="index"
+        :sortable='item.prop===defaultSort.prop'
+        align="center"
+        :prop="item.prop"
+        :label="item.label"
+        :width="item.width">
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props:{
+    tableData: {
+      default: [],
+    },
+    tableHeader: {
+      default: [],
+    },
+    defaultSort: {
+      default: {},
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
